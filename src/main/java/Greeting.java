4a90b51cd018b7e0aa13d0881ba8c2a7035afdc4
Charlie Agarwal <chirayush.agarwal@aa.com>
2018-07-02 07:23:46 -0500

    Implementing upto Requirement 4

--- src/main/java/Greeting.java
+++ src/main/java/Greeting.java
@@ -1,2 +1,18 @@
-public class Greeting {
+import org.apache.commons.lang3.StringUtils;
+
+final class Greeting {
+    String greet(final String name) {
+        if (StringUtils.isEmpty(name)) {
+            return sayHello("my friend");
+        }
+
+        if (StringUtils.isAllUpperCase(name)) {
+            return "HELLO " + name + "!";
+        }
+        return sayHello(name);
+    }
+
+    private String sayHello(String name) {
+        return "Hello, " + name + ".";
+    }
 }
--- src/test/java/GreetingTest.java
+++ src/test/java/GreetingTest.java
@@ -1,2 +1,37 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class GreetingTest {
+    Greeting greeting = new Greeting();
+
+    @Test
+    public void greetShouldReturnNameForSimpleGreetingBob() {
+        assertEquals("Hello, Bob.", greeting.greet("Bob"));
+    }
+
+    @Test
+    public void greetShouldReturnNameForSimpleGreetingRam() {
+        assertEquals("Hello, Ram.", greeting.greet("Ram"));
+    }
+
+    @Test
+    public void greetShouldReturnMyFriendWhenNameIsEmpty() {
+        assertEquals("Hello, my friend.", greeting.greet(""));
+    }
+
+    @Test
+    public void greetShouldReturnMyFriendWhenNameIsNull() {
+        assertEquals("Hello, my friend.", greeting.greet(null));
+    }
+
+    @Test
+    public void greetShouldGreetInUppercaseWhenNameIsUppercaseJERRY() {
+        assertEquals("HELLO JERRY!", greeting.greet("JERRY"));
+    }
+
+    @Test
+    public void greetShouldGreetInUppercaseWhenNameIsUppercaseJOE() {
+        assertEquals("HELLO JOE!", greeting.greet("JOE"));
+    }
 }
