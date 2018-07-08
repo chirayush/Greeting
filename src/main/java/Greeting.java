36673400ec959ff224196d73acf98a2f8007f0f6
Charlie Agarwal <chirayush.agarwal@aa.com>
2018-07-08 11:52:08 -0500

    Implementing upto Requirement 7

--- src/main/java/Greeting.java
+++ src/main/java/Greeting.java
@@ -1,2 +1,75 @@
-public class Greeting {
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+final class Greeting {
+
+    private static final String HELLO = "Hello, ";
+    private static final String PERIOD = ".";
+
+    String greet(final String... names) {
+        if (names == null || names[0].isEmpty()) {
+            return HELLO + "my friend.";
+        }
+        List<String> normalGreetNames = getNormalGreetNames(names);
+
+        StringBuilder greetingBuilder = new StringBuilder();
+        if (!normalGreetNames.isEmpty()) {
+            buildNormalGreeting(normalGreetNames, greetingBuilder);
+        }
+
+        String shoutingName = getShoutingName(names);
+        if (!normalGreetNames.isEmpty() && !shoutingName.isEmpty()) {
+            greetingBuilder.append(" AND ");
+        }
+
+        if (!shoutingName.isEmpty()) {
+            greetingBuilder.append("HELLO ")
+                    .append(shoutingName)
+                    .append("!");
+        }
+
+        return greetingBuilder.toString();
+    }
+
+    private void buildNormalGreeting(List<String> normalGreetNames, StringBuilder greetingBuilder) {
+        greetingBuilder.append(HELLO);
+
+        final int greetNameSize = normalGreetNames.size();
+        if (greetNameSize == 1) {
+            greetingBuilder.append(normalGreetNames.get(0));
+        } else if (greetNameSize == 2) {
+            greetingBuilder.append(normalGreetNames.get(0))
+                    .append(" and ")
+                    .append(normalGreetNames.get(1));
+        } else {
+            for (int i = 0; i < greetNameSize - 1; i++) {
+                greetingBuilder.append(normalGreetNames.get(i)).append(", ");
+            }
+
+            greetingBuilder.append("and ")
+                    .append(getLastName(normalGreetNames));
+        }
+
+        greetingBuilder.append(PERIOD);
+    }
+
+    private List<String> getNormalGreetNames(String[] names) {
+        return Arrays.stream(names)
+                .filter(name -> !StringUtils.isAllUpperCase(name))
+                .collect(Collectors.toList());
+    }
+
+    private String getShoutingName(String[] names) {
+        return Arrays.stream(names)
+                .filter(StringUtils::isAllUpperCase)
+                .findFirst()
+                .orElse("");
+    }
+
+    private String getLastName(List<String> normalGreetNames) {
+        return normalGreetNames.get(normalGreetNames.size() - 1);
+    }
 }
--- src/test/java/GreetingTest.java
+++ src/test/java/GreetingTest.java
@@ -1,2 +1,68 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class GreetingTest {
+    private Greeting greeting = new Greeting();
+
+    @Test
+    public void greetShouldReturnNameForSimpleGreetingBob() {
+        assertEquals("Hello, Bob.", greeting.greet("Bob"));
+    }
+
+    @Test
+    public void greetShouldReturnNameForSimpleGreetingRam() {
+        assertEquals("Hello, Ram.", greeting.greet("Ram"));
+    }
+
+    @Test
+    public void greetShouldReturnMyFriendWhenNameIsEmpty() {
+        assertEquals("Hello, my friend.", greeting.greet(""));
+    }
+
+    @Test
+    public void greetShouldReturnMyFriendWhenNameIsNull() {
+        assertEquals("Hello, my friend.", greeting.greet(null));
+    }
+
+    @Test
+    public void greetShouldGreetInUppercaseWhenNameIsUppercaseJERRY() {
+        assertEquals("HELLO JERRY!", greeting.greet("JERRY"));
+    }
+
+    @Test
+    public void greetShouldGreetInUppercaseWhenNameIsUppercaseJOE() {
+        assertEquals("HELLO JOE!", greeting.greet("JOE"));
+    }
+
+    @Test
+    public void greetShouldGreet2Names() {
+        assertEquals("Hello, Jill and Jane.", greeting.greet("Jill", "Jane"));
+    }
+
+    @Test
+    public void greetShouldGreet3Names() {
+        assertEquals("Hello, Amy, Brian, and Charlotte.", greeting.greet("Amy", "Brian", "Charlotte"));
+    }
+
+    @Test
+    public void greetShouldGreet4Names() {
+        assertEquals("Hello, Amy, Brian, Charlie, and Charlotte.", greeting.greet("Amy", "Brian", "Charlie", "Charlotte"));
+    }
+
+    @Test
+    public void greetShouldMixShoutingAndNormalMixing() {
+        assertEquals("Hello, Amy and Charlotte. AND HELLO BRIAN!", greeting.greet("Amy", "BRIAN", "Charlotte"));
+    }
+
+    @Test
+    public void greetShouldSeparateOutCommaSeparatedNames() {
+        assertEquals("Hello, Bob, Charlie, and Dianne.", greeting.greet("Bob", "Charlie, Dianne"));
+    }
+
+//    @Test
+//    public void greetShouldSkipCommaSeparationWhenEscapedByDoubleQuotes() {
+//        assertEquals("Hello, Bob and Charlie, Dianne.", greeting.greet("Bob", "\"Charlie, Dianne\""));
+//    }
 }
+
